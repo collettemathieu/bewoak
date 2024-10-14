84773010de01b55d429b7696c29c3056e9aaa362
Mathieu Collette <collettemathieu@jsonata.com>
2024-10-14 18:42:15 +0200

    fix: business rules

--- libs/pathway-design/common/business/pathway/rules/src/lib/title/title.rules.ts
+++ libs/pathway-design/common/business/pathway/rules/src/lib/title/title.rules.ts
@@ -4,8 +4,8 @@ export const PDCBPR_titleRules: Rules = {
     textError: function () {
         return `Renseigner un nom entre 1 et ${this.maxLength} caractères.`;
     },
-    isValid: function (value: string | null) {
-        return value !== null && value.trim().length !== 0 && value.trim().length <= this.maxLength;
+    isValid: function (value: string | null | undefined) {
+        return value !== undefined && value !== null && value.trim().length !== 0 && value.trim().length <= this.maxLength;
     },
     isRequired: true,
     maxLength: 100,
