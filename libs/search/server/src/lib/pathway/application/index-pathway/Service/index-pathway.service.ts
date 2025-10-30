5987035c75aaf08cee19954c744ec2323d9b88ad
Mathieu Collette <collettemathieu@jsonata.com>
2025-10-30 21:12:00 +0100

    feat: implement the first step for the pathway initialized event in search domain

--- libs/search/server/src/lib/pathway/application/index-pathway/Service/index-pathway.service.ts
+++ libs/search/server/src/lib/pathway/application/index-pathway/Service/index-pathway.service.ts
@@ -2,9 +2,10 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class IndexPathwayService {
+    // biome-ignore lint/suspicious/noExplicitAny: <explanation>
     async indexPathway(pathwayData: any): Promise<void> {
         // Implement indexing logic here
-        // biome-ignore lint/suspicious/noConsole: <explanation>
+        // biome-ignore lint/suspicious/noConsole: <Not pertinent>
         console.log('Indexing pathway data:', pathwayData);
     }
 }
